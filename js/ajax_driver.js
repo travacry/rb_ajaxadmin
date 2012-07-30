b32703781de7025774bacf71c651fe4b6d87eeb2
travacry <ups201@yandex.ru>
2012-07-30 01:03:36 -0700

    for control ajax request's + select page

---
+++ js/ajax_driver.js
@@ -0,0 +1,154 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+	create 2012, travacry(ajax, transfer packets)
+*/
+function ajax_driver(){
+	/*
+	 example:
+
+	 //http://api.restobank.com/api.php?method=company.get&limit=1
+	 var ajaxObj_companyGet_limit1 = {    id : 1, pack_id : 1,    data :  {   method: "company.get",  limit: 1}   }
+	 var ajaxObj_companyGet_limit2 = {    id : 2, pack_id : 1,    data :  {   method: "company.get",  limit: 2}   }
+
+	 var ajax_dr = new ajax_driver();
+	 ajax_dr.addPack(1);
+	 ajax_dr.addReq(ajaxObj_companyGet_limit1);
+	 ajax_dr.addReq(ajaxObj_companyGet_limit2);
+
+	 //ajax_dr.setCbOKReq(1,2, function(data){ console.dir(data); });
+	 ajax_dr.setCbOkPack(1, function(data){ console.dir(data); });
+	 ajax_dr.setCbErrReq(1,1, function(xhr, textStatus){ console.log(textStatus); });
+
+	 ajax_dr.sendPack(1);
+
+	 */
+
+	var self = this;
+	self.pack = {};
+
+	self.addPack = function(pack_id){
+		if (typeof(pack_id) != "undefined"){
+			self.pack[pack_id] = {};
+			self.pack[pack_id]._timeout = 180000;
+			self.pack[pack_id]._url = "http://api.restobank.com/api.php";
+			self.pack[pack_id]._type = "POST";
+			self.pack[pack_id]._dataType = "json";
+			self.pack[pack_id]._cache = false;
+		}
+	}
+
+	self.addReq = function(req_obj){
+
+		/*
+		 id, pack_id, url, type(get,post), timeout, method1, method2
+		 */
+		if(typeof(req_obj) == "object"){
+			if ((typeof(req_obj.pack_id) != "undefined") && (typeof(self.pack[req_obj.pack_id][req_obj.id]) == "undefined")){
+
+				self.pack[req_obj.pack_id][req_obj.id] = {};
+
+				self.pack[req_obj.pack_id][req_obj.id].id;
+				self.pack[req_obj.pack_id][req_obj.id].pack_id;
+				self.pack[req_obj.pack_id][req_obj.id].url = self.pack[req_obj.pack_id]._url;
+				self.pack[req_obj.pack_id][req_obj.id].type = self.pack[req_obj.pack_id]._type;
+				self.pack[req_obj.pack_id][req_obj.id].timeout = self.pack[req_obj.pack_id]._timeout;
+				self.pack[req_obj.pack_id][req_obj.id].data;
+				self.pack[req_obj.pack_id][req_obj.id].dataType = self.pack[req_obj.pack_id]._dataType;
+				self.pack[req_obj.pack_id][req_obj.id].cache = self.pack[req_obj.pack_id]._cache;
+				self.pack[req_obj.pack_id][req_obj.id].success;
+				self.pack[req_obj.pack_id][req_obj.id].error;
+
+				for (var next in req_obj){
+					if ((next == "pack_id")||(next == "id")) continue;
+					self.pack[req_obj.pack_id][req_obj.id][next] = req_obj[next];
+				}
+
+				return true;
+			}
+		}
+		return false
+	}
+
+
+	self.deletePack = function(pack_id){
+		if (typeof(pack_id) != "undefined")
+			return delete self.pack[pack_id];
+		else
+			return false;
+	}
+
+	self.clearReq = function(pack_id, req_id){
+		if (typeof(req_id) != "undefined")
+			return delete self.pack[pack_id][req_id];
+		else
+			return false;
+	}
+
+	self.setReqTimeout = function(pack_id, req_id, timeout){
+		self.pack[pack_id][req_id].timeout = timeout;
+	}
+
+	self.setPackTimeout = function(pack_id, timeout){
+		for (var next in self.pack[pack_id]){
+			self.pack[pack_id][next].timeout = timeout;
+		}
+	}
+
+	self.sendReq = function(pack_id, req_id){
+		if (typeof(pack_id) != "undefined"){
+			if (typeof(req_id) != "undefined"){
+				$.ajax({
+					type: self.pack[pack_id][req_id].type,
+					url: self.pack[pack_id][req_id].url,
+					data: self.pack[pack_id][req_id].data,
+					dataType: self.pack[pack_id][req_id].dataType,
+					cache: self.pack[pack_id][req_id].cache,
+					timeout: self.pack[pack_id][req_id].timeout, //3min
+					success: self.pack[pack_id][req_id].success
+				})
+				return true;
+			}
+			return false;
+		}
+	}
+
+	self.sendPack = function(pack_id){
+		if (typeof(pack_id) != "undefined"){
+			for (var next in self.pack[pack_id]){
+				$.ajax({
+					type: self.pack[pack_id][next].type,
+					url: self.pack[pack_id][next].url,
+					data: self.pack[pack_id][next].data,
+					dataType: self.pack[pack_id][next].dataType,
+					cache: self.pack[pack_id][next].cache,
+					timeout: self.pack[pack_id][next].timeout, //3min
+					success: self.pack[pack_id][next].success
+				})
+			}
+			return true;
+		} else
+			return false;
+	}
+
+
+	//OK
+	self.setCbOKReq = function(pack_id, req_id, cb){
+		self.pack[pack_id][req_id].success = cb;
+	}
+
+	self.setCbOkPack = function(pack_id, cb){
+		for (var next in self.pack[pack_id])
+			self.pack[pack_id][next].success = cb;
+	}
+
+	//error , xhr, textStatus
+	self.setCbErrReq = function(pack_id, req_id, cb){
+		self.pack[pack_id][req_id].error = cb;
+	}
+
+	self.setCbErrPack = function(pack_id, cb){
+		for (var next in self.pack[pack_id])
+			self.pack[pack_id][next].error = cb;
+	}
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
---
+++ js/manager_page.js
@@ -0,0 +1,428 @@
+init_data = {};
+
+function manager_page(){
+	var ajax_self = this;
+	ajax_self.data = {};
+
+	ajax_self.id = 0;
+	ajax_self.mod = new Model();
+
+	//для наглядности
+	/*
+	 self.base_info;
+	 self.imformation_about_company;
+	 self.information_about_address;
+	 self.time_work_res;
+	 self.location;
+	 */
+
+	ajax_self.test = function(){
+	}
+
+	//число повторных проверок валидности ответов
+	ajax_self.count_endCreateModelPage = 0;
+
+	ajax_self.endCreateModelPage = function(namePage, data_company, id_address){
+
+		if (ajax_self.count_endCreateModelPage == 3){
+			clearInterval(ajax_self.intervalID);
+			console.log("error");
+		}
+
+		if((ajax_self.mod.count_request["" + namePage] == ajax_self.mod.count_response["" + namePage])&&(dataModel.pid.Ajax > 0)){
+			clearInterval(ajax_self.intervalID);
+			//ответ валиден
+			ajax_dataModel.createPageModel(namePage, data_company, id_address);
+		}
+		++ajax_self.count_endCreateModelPage;
+	}
+
+	/*
+	 self.CreateViewPage = function(namePage){
+	 //данные для станиы сформированны
+	 //котроллеры для страниц
+	 switch(namePage){
+	 case: "information":
+
+	 break;
+	 }
+	 }
+	 */
+
+	ajax_self.AddDataAjax = function(data, nameMethod){
+
+		//добавление данных в модель
+		for(name in data){
+			ajax_self.data[nameMethod] = {};
+			ajax_self.data[nameMethod][name] = data["" + name];
+		}
+	}
+
+
+	ajax_self.intervalID = "";
+	ajax_self.beginCreateModelPage = function(namePage, id_address, id_company, data_company){
+		//начало вызова
+		ajax_self.data = {};
+		//ajax вызовы
+		switch(namePage){
+			case "news":
+				break;
+			case "menu":
+				break;
+			case "information":
+
+				if (typeof(ajax_self.intervalID) != "undefined"){
+					clearInterval(ajax_self.intervalID);
+				}
+
+				param1 = {};
+
+				dataModel.pid.Ajax = 0;
+
+
+				param1.name = "limit";
+				param1.val = 2;
+				ajax_self.mod.driver_ajax(1, ajax_self.AddDataAjax, "information", "company.get", param1);
+
+				//не пашет
+				param1.name = "company_ids";
+				param1.val = id_company;
+				ajax_self.mod.driver_ajax(1, ajax_self.AddDataAjax, "information", "address.get", param1);
+
+				if (dataModel.pid.Ajax == 0){
+					ajax_self.intervalID  = setInterval(function(){
+						ajax_self.endCreateModelPage(namePage, data_company, id_address);
+						dataModel.pid.Ajax = 1;
+					} ,2000); //повторение проверки через 30 сек * 3 1.5 мин на ожидание
+				}
+
+				break;
+			case "reviews":
+				break;
+			case "interior":
+				break;
+			case "clients":
+				break;
+			case "settings":
+				break;
+			case "photoreports":
+				break;
+
+		}
+	}
+
+	var ajax_dataModel = {};
+
+	ajax_dataModel.createPageModel = function(namePage, data_company, id_address){
+		console.log("PID ", dataModel.pid.Ajax);
+		//добавление данных в модель
+		//							кеширование данных
+
+		/* for(name in data){
+		 ajax_dataModel.data["" + name] = data["" + name];
+		 } */
+		if (typeof(dataModel["" + namePage]) == "undefined"){
+			dataModel["" + namePage] = {};
+		}
+
+		switch(namePage){
+			case "news":
+				/*
+				 company.get
+				 dataModel.base_info.logo = data.response[0].logo[114];
+				 dataModel.base_info.foto = ;
+				 dataModel.company_list = ;
+				 dataModel.address_list;
+				 */
+				break;
+			case "menu":
+				break;
+			case "information":
+				//ответ на прямой запрос
+
+				if (dataModel.pid.Ajax == 1){
+
+					var data_address = 0;
+					for (var next in ajax_self.data["address.get"].response){
+						if (ajax_self.data["address.get"].response[next].id == id_address){
+							data_address = ajax_self.data["address.get"].response[next];
+							console.log("data_address ", data_address);
+						}
+					}
+
+					/* Компания */
+
+					//Язык - [] без [0]
+					dataModel[namePage].language = {};
+					dataModel[namePage].language.main = data_company.language[0];
+					dataModel[namePage].language.list = data_company.language;
+
+
+					//Название
+					dataModel[namePage].name_company = data_company.name;
+					//Описание
+					dataModel[namePage].description_company = data_company.description;
+					//Юридическое лицо ?
+					dataModel[namePage].juridical_person = "";
+
+					//Рейтинг среднего счета без нал. (сумма)
+					dataModel[namePage].reiting_no_cash = data_company.check;
+
+					/*
+					 dataModel[namePage].language = {};
+					 dataModel[namePage].language.main = ajax_self.data["company.get"].response[0].language.shift();
+					 dataModel[namePage].language.list = ajax_self.data["company.get"].response[0].language;
+
+					 //Название
+					 dataModel[namePage].name_company = ajax_self.data["company.get"].response[0].name;
+					 //Описание
+					 dataModel[namePage].description_company = ajax_self.data["company.get"].response[0].description;
+					 //Юридическое лицо ?
+					 dataModel[namePage].juridical_person = "";
+					 //Средний счет без нал.
+					 dataModel[namePage].no_cash = ajax_self.data["company.get"].response[0].check;
+					 //Вся валюта системы
+					 dataModel[namePage].no_cash = ajax_self.data["currency.get"].response[0].sign; //obj id=1, руб.
+					 //Валюта ресторана ?
+					 dataModel[namePage].money_unit = "";
+					 //Рейтинг среднего счета без нал. (пока костыль ?)
+					 dataModel[namePage].reiting_no_cash = ajax_self.data["company.get"].response[0].check;
+					 */
+					/* Адрес */
+
+					//Дата открытия (null ? ?? )
+					dataModel[namePage].opening_date = data_address.opening_date;
+					//Сайт (!!! косяк -> адрес)
+					dataModel[namePage].site = data_company.site;
+					//Телефон (у компании есть ?)
+					dataModel[namePage].phone = {};
+					dataModel[namePage].phone = data_address.phone;
+					dataModel[namePage].phone.list = {};
+
+					if (typeof(data_address.phone[0]) != "undefined"){
+						for(var next in data_address.phone){
+							if (data_address.phone[next].type == 1){
+								//основной телефон
+								dataModel[namePage].phone.main = data_address.phone[next].phone;
+							} else {
+								for (var next_type in init_data["phoneType.get"].response){
+									if (data_address.phone[next].type == init_data["phoneType.get"].response[next_type].type){
+										var name_type = init_data["phoneType.get"].response[next_type].name[dataModel[namePage].language.main];
+										//все телефоны, -"Основной"
+										dataModel[namePage].phone.list[name_type] = data_address.phone[next].phone;
+									}
+								}
+							}
+						}
+					}
+					//Индекс
+					dataModel[namePage].postal_code = data_address.address.postal_code;
+					//Описание
+					dataModel[namePage].description_address = data_address.description;
+					//Кол-во посадочных мест :
+					//Ресторан:
+					dataModel[namePage].capacity_indoor = data_address.capacity.indoor;
+					//Терраса:
+					dataModel[namePage].capacity_outdoor = data_address.capacity.outdoor;
+					//Всего п.м
+					dataModel[namePage].capacity_all = dataModel[namePage].capacity_indoor + dataModel[namePage].capacity_outdoor;
+
+
+					//Адрес:
+					dataModel[namePage].street_list = data_address.address.street;
+
+					//Время работы (ресторана):
+
+					dataModel[namePage].time_work = {};
+					dataModel[namePage].time_work.res = {};
+
+
+					//init
+					dataModel[namePage].time_work.res.Monday = {};
+					dataModel[namePage].time_work.res.Monday.begin = "";
+					dataModel[namePage].time_work.res.Monday.end = "";
+					dataModel[namePage].time_work.res.Monday.kitchen = "";
+					dataModel[namePage].time_work.res.Tuesday = {};
+					dataModel[namePage].time_work.res.Tuesday.begin = "";
+					dataModel[namePage].time_work.res.Tuesday.end = "";
+					dataModel[namePage].time_work.res.Tuesday.kitchen = "";
+					dataModel[namePage].time_work.res.Wednesday = {};
+					dataModel[namePage].time_work.res.Wednesday.begin = "";
+					dataModel[namePage].time_work.res.Wednesday.end = "";
+					dataModel[namePage].time_work.res.Wednesday.kitchen = "";
+					dataModel[namePage].time_work.res.Thursday = {};
+					dataModel[namePage].time_work.res.Thursday.begin = "";
+					dataModel[namePage].time_work.res.Thursday.end = "";
+					dataModel[namePage].time_work.res.Thursday.kitchen = ""
+					dataModel[namePage].time_work.res.Friday = {};
+					dataModel[namePage].time_work.res.Friday.begin = "";
+					dataModel[namePage].time_work.res.Friday.end = "";
+					dataModel[namePage].time_work.res.Friday.kitchen = "";
+					dataModel[namePage].time_work.res.Saturday = {};
+					dataModel[namePage].time_work.res.Saturday.begin = "";
+					dataModel[namePage].time_work.res.Saturday.end = "";
+					dataModel[namePage].time_work.res.Saturday.kitchen = "";
+					dataModel[namePage].time_work.res.Sunday = {};
+					dataModel[namePage].time_work.res.Sunday.begin = "";
+					dataModel[namePage].time_work.res.Sunday.end = "";
+					dataModel[namePage].time_work.res.Sunday.kitchen = "";
+
+
+					//Понедельник
+					if (typeof(data_address.worktime[1][0]) != "undefined"){
+						if (typeof(data_address.worktime[1][0][0]) != "undefined")
+							dataModel["" + namePage].time_work.res.Monday.begin = ajax_self.getformatSM(data_address.worktime[1][0][0]);
+						if (typeof(data_address.worktime[1][0][0]) != "undefined")
+							dataModel["" + namePage].time_work.res.Monday.end = ajax_self.getformatSM(data_address.worktime[1][0][1]);
+					}
+					if (typeof(data_address.worktime[1][1]) != "undefined"){
+						if (typeof(data_address.worktime[1][1][0]) != "undefined")
+							dataModel["" + namePage].time_work.res.Monday.kitchen = ajax_self.getformatSM(data_address.worktime[1][1][0]);
+					}
+					//Вторник
+					if (typeof(data_address.worktime[2][0]) != "undefined"){
+						if (typeof(data_address.worktime[2][0][0]) != "undefined")
+							dataModel["" + namePage].time_work.res.Tuesday.begin = ajax_self.getformatSM(data_address.worktime[2][0][0]);
+						if (typeof(data_address.worktime[2][0][1]) != "undefined")
+							dataModel["" + namePage].time_work.res.Tuesday.end = ajax_self.getformatSM(data_address.worktime[2][0][1]);
+					}
+					if (typeof(data_address.worktime[2][1]) != "undefined"){
+						if (typeof(data_address.worktime[2][1][0]) != "undefined")
+							dataModel["" + namePage].time_work.res.Tuesday.kitchen = ajax_self.getformatSM(data_address.worktime[2][1][0]);
+					}
+					//Среда
+					if (typeof(data_address.worktime[3][0]) != "undefined"){
+						if (typeof(data_address.worktime[3][0][0]) != "undefined")
+							dataModel["" + namePage].time_work.res.Wednesday.begin = ajax_self.getformatSM(data_address.worktime[3][0][0]);
+						if (typeof(data_address.worktime[3][0][1]) != "undefined")
+							dataModel["" + namePage].time_work.res.Wednesday.end = ajax_self.getformatSM(data_address.worktime[3][0][1]);
+					}
+					if (typeof(data_address.worktime[3][1]) != "undefined"){
+						if (typeof(data_address.worktime[3][1][0]) != "undefined")
+							dataModel["" + namePage].time_work.res.Wednesday.kitchen = ajax_self.getformatSM(data_address.worktime[3][1][0]);
+					}
+					//Четверг
+					if (typeof(data_address.worktime[4][0]) != "undefined"){
+						if (typeof(data_address.worktime[4][0][0]) != "undefined")
+							dataModel["" + namePage].time_work.res.Thursday.begin = ajax_self.getformatSM(data_address.worktime[4][0][0]);
+						if (typeof(data_address.worktime[4][0][1]) != "undefined")
+							dataModel["" + namePage].time_work.res.Thursday.end = ajax_self.getformatSM(data_address.worktime[4][0][1]);
+					}
+					if (typeof(data_address.worktime[4][1]) != "undefined"){
+						if (typeof(data_address.worktime[4][1][0]) != "undefined")
+							dataModel["" + namePage].time_work.res.Thursday.kitchen = ajax_self.getformatSM(data_address.worktime[4][1][0]);
+					}
+					//Пятница
+					if (typeof(data_address.worktime[5][0]) != "undefined"){
+						if (typeof(data_address.worktime[5][0][0]) != "undefined")
+							dataModel["" + namePage].time_work.res.Friday.begin = ajax_self.getformatSM(data_address.worktime[5][0][0]);
+						if (typeof(data_address.worktime[5][0][1]) != "undefined")
+							dataModel["" + namePage].time_work.res.Friday.end = ajax_self.getformatSM(data_address.worktime[5][0][1]);
+					}
+					if (typeof(data_address.worktime[5][1]) != "undefined"){
+						if (typeof(data_address.worktime[5][1][0]) != "undefined")
+							dataModel["" + namePage].time_work.res.Friday.kitchen = ajax_self.getformatSM(data_address.worktime[5][1][0]);
+					}
+					//Суббота
+					if (typeof(data_address.worktime[6][0]) != "undefined"){
+						dataModel["" + namePage].time_work.res.Saturday = {};
+						if (typeof(data_address.worktime[6][0][0]) != "undefined")
+							dataModel["" + namePage].time_work.res.Saturday.begin = ajax_self.getformatSM(data_address.worktime[6][0][0]);
+						if (typeof(data_address.worktime[6][0][1]) != "undefined")
+							dataModel["" + namePage].time_work.res.Saturday.end = ajax_self.getformatSM(data_address.worktime[6][0][1]);
+					}
+					if (typeof(data_address.worktime[6][1]) != "undefined"){
+						if (typeof(data_address.worktime[6][1][0]) != "undefined")
+							dataModel["" + namePage].time_work.res.Saturday.kitchen = ajax_self.getformatSM(data_address.worktime[6][1][0]);
+					}
+					//Воскресенье
+					if (typeof(data_address.worktime[0][0]) != "undefined"){
+						dataModel["" + namePage].time_work.res.Sunday = {};
+						if (typeof(data_address.worktime[0][0][0]) != "undefined")
+							dataModel["" + namePage].time_work.res.Sunday.begin = ajax_self.getformatSM(data_address.worktime[0][0][0]);
+						if (typeof(data_address.worktime[0][0][1]) != "undefined")
+							dataModel["" + namePage].time_work.res.Sunday.end = ajax_self.getformatSM(data_address.worktime[0][0][1]);
+					}
+					if (typeof(data_address.worktime[6][1]) != "undefined"){
+						if (typeof(data_address.worktime[6][1][1]) != "undefined")
+							dataModel["" + namePage].time_work.res.Sunday.kitchen = ajax_self.getformatSM(data_address.worktime[6][1][1]);
+					}
+
+					//Долгота
+					dataModel["" + namePage].longitude = data_address.address.longitude;
+					//Широта
+					dataModel["" + namePage].latitude = data_address.address.latitude;
+
+
+					//Город:
+					for(var res in init_data["city.get"].response){
+						if (init_data["city.get"].response[res].id == data_address.address.city_id){
+							dataModel["" + namePage].city = init_data["city.get"].response[res].name;
+						}
+					}
+
+					//Страна:
+					for(var res in init_data["country.get"].response){
+						if (init_data["country.get"].response[res].id == data_address.address.country_id){
+							dataModel["" + namePage].country = init_data["country.get"].response[res].name;
+						}
+					}
+
+					//Метро:
+					var count = 0;
+					dataModel[namePage].subway = {};
+					dataModel[namePage].subway.list = [];
+					for(var res in init_data["subway.get"].response){
+						for (var next in ajax_self.data["address.get"].response[0].address.subway_id){
+							if (init_data["subway.get"].response[res].id == data_address.address.subway_id[next]){
+								dataModel["" + namePage].subway.list[count] = init_data["subway.get"].response[res].name;
+								++count;
+							}
+						}
+					}
+					//основное метро
+					dataModel[namePage].subway.main = dataModel[namePage].subway.list.shift();
+
+
+					//Валюта ресторана:
+					var resto_val = init_data["currency.get"].response[0].sign;	//.руб	(пока обьект 1)
+					//Средний счет без нал.:
+					dataModel[namePage].no_cash = data_company.price.check + " " + resto_val;
+
+					//завершение запросов
+					dataModel.pid.Ajax = 0;
+
+					$("#textAddress").trigger('update_info',[ namePage, dataModel[namePage] ]);
+					console.log(" --- END --- ");
+					//dataModel.updateViewModel();
+				}
+
+
+				break;
+			case "reviews":
+				break;
+			case "interior":
+				break;
+			case "clients":
+				break;
+			case "settings":
+				break;
+			case "photoreports":
+				break;
+		}
+
+
+
+	}
+	ajax_self.getformatSM = function(sec){
+		var minutes = parseInt(sec / 60);
+		var seconds = sec % 60;
+		var hours = parseInt(minutes / 60);
+		minutes = minutes % 60;
+		// Обеспечиваем ведущие нули
+		if(String(minutes).length != 2) minutes = "0" + minutes;
+		if(String(seconds).length != 2) seconds = "0" + seconds;
+		// Выводим результат
+		return hours + ":" + minutes;
+	}
+}
